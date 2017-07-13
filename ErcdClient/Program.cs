c969c4995e11d7541545a78e9ceaf3f1623ef0b7
solapvk <solapvk@hotmail.com>
2017-07-13 15:34:41 +0800

    demo

---
+++ ErcdClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using Google.Protobuf;
+using Grpc.Core;
+using System;
+using static Etcdserverpb.KV;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel c = new Channel("localhost", 2379, ChannelCredentials.Insecure);
+            KVClient kv = new KVClient(c);
+
+            Console.WriteLine("client");
+
+            while (true)
+            {
+                Console.WriteLine("...");
+
+                var sss = Console.ReadLine();
+                if (sss.StartsWith("get "))
+                {
+                    sss = sss.Replace("get ", "").TrimStart();
+
+                    var lis = kv.Range(new Etcdserverpb.RangeRequest() { Key = ByteString.CopyFromUtf8(sss) });
+                    Console.WriteLine(lis.ToString());
+                }
+                else if (sss.StartsWith("set "))
+                {
+                    sss = sss.Replace("set ", "").TrimStart();
+                    var sasa = sss.Split(' ');
+                    var res = kv.Put(new Etcdserverpb.PutRequest() { Key = Google.Protobuf.ByteString.CopyFromUtf8(sasa[0]), Value = ByteString.CopyFromUtf8(sasa[1]) });
+                    Console.WriteLine(res.ToString());
+
+                }
+            }
+
+        }
+    }
+}
---
+++ EtcdWatch/Program.cs
@@ -0,0 +1,47 @@
+﻿using Etcdserverpb;
+using Grpc.Core;
+using System;
+using static Etcdserverpb.Watch;
+using static Etcdserverpb.WatchCreateRequest.Types;
+
+namespace Etcd
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Channel c = new Channel("localhost", 2379, ChannelCredentials.Insecure);
+            WatchClient cli = new WatchClient(c);
+
+            var x = cli.Watch();
+            Console.WriteLine("input key....");
+            var key = Console.ReadLine();
+
+            var watchAction = new Action(async () =>
+            {
+                await x.RequestStream.WriteAsync(new WatchRequest() { CreateRequest = new WatchCreateRequest() { ProgressNotify = false, Key = Google.Protobuf.ByteString.CopyFromUtf8(key), PrevKv = true } });
+                await x.RequestStream.CompleteAsync();
+
+            });
+            watchAction();
+            
+            var eventOn = new Action(async () =>
+            {
+                while (await x.ResponseStream.MoveNext(new System.Threading.CancellationToken()))
+                {
+                    Console.WriteLine(x.ResponseStream.Current.ToString());
+                    foreach (var item in x.ResponseStream.Current.Events)
+                    {
+                        Console.WriteLine(item.Kv.ToString());
+                    }
+
+                }
+            });
+            eventOn();
+
+            Console.WriteLine("...");
+            Console.ReadLine();
+        }
+    }
+}
